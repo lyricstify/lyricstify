887a888a7143a063fc86446bfca3f63cd9ca7d14
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-09 20:50:43 +0700

    chore(repl): add non terminal service to repl

--- src/repl.module.ts
+++ src/repl.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthorizationModule } from './authorization/authorization.module.js';
 import { ClientModule } from './client/client.module.js';
 import { LyricModule } from './lyric/lyric.module.js';
+import { PlayerModule } from './player/player.module.js';
 import { RefreshTokenModule } from './refresh-token/refresh-token.module.js';
+import { TerminalKitModule } from './terminal-kit/terminal-kit.module.js';
 import { TokenModule } from './token/token.module.js';
+import { TransformationModule } from './transformation/transformation.module.js';
 
 @Module({
   imports: [
@@ -12,6 +15,10 @@ import { TokenModule } from './token/token.module.js';
     RefreshTokenModule,
     TokenModule,
     LyricModule,
+    PlayerModule,
+    TokenModule,
+    TransformationModule,
+    TerminalKitModule,
   ],
 })
 export class ReplModule {}
