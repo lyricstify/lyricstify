0c1389646238a4c954e3fea0a35d18869d586e5f
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-11 23:28:38 +0700

    fix(rxjs): change incorrect error message

--- src/common/rxjs/operators/throw-axios-error-response-if-available.operator.ts
+++ src/common/rxjs/operators/throw-axios-error-response-if-available.operator.ts
@@ -15,15 +15,12 @@ export const throwAxiosErrorResponseIfAvailable = <
         throwError(
           () =>
             new Error(
-              `Failed to get refresh token data. Message: ${
-                (typeof error.response?.data.error === 'object'
-                  ? error.response?.data.error.message
-                  : '') ||
+              (typeof error.response?.data.error === 'object'
+                ? JSON.stringify(error.response?.data.error.message)
+                : error.response?.data.error) ||
                 error.response?.data.error_description ||
-                error.response?.data.error ||
                 error.response?.statusText ||
-                error.message
-              }`,
+                error.message,
             ),
         ),
     ),
