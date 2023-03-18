07e04a4382af52a214e612e519bfc99507b25a60
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-18 17:43:40 +0700

    chore(repl): load non commands and non terminal module

--- src/repl.module.ts
+++ src/repl.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthorizationModule } from './authorization/authorization.module.js';
 import { ClientModule } from './client/client.module.js';
+import { CommandValidationModule } from './command-validation/command-validation.module.js';
+import { ConfigModule } from './config/config.module.js';
 import { LyricModule } from './lyric/lyric.module.js';
 import { PlayerModule } from './player/player.module.js';
 import { RefreshTokenModule } from './refresh-token/refresh-token.module.js';
-import { TerminalKitModule } from './terminal-kit/terminal-kit.module.js';
 import { TokenModule } from './token/token.module.js';
 import { TransformationModule } from './transformation/transformation.module.js';
 
@@ -12,13 +13,13 @@ import { TransformationModule } from './transformation/transformation.module.js'
   imports: [
     AuthorizationModule,
     ClientModule,
-    RefreshTokenModule,
-    TokenModule,
+    CommandValidationModule,
+    ConfigModule,
     LyricModule,
     PlayerModule,
+    RefreshTokenModule,
     TokenModule,
     TransformationModule,
-    TerminalKitModule,
   ],
 })
 export class ReplModule {}
