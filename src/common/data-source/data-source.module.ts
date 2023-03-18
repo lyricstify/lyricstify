9abfb508b108c5c320868e5a4f21f5db5aa653a2
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-18 16:52:03 +0700

    fix(data-source): change data source provider implementation

--- src/common/data-source/data-source.module.ts
+++ src/common/data-source/data-source.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { DataSourceService } from './data-source.service.js';
 
 @Module({
-  providers: [DataSourceService],
+  providers: [
+    {
+      provide: DataSourceService,
+      useValue: new DataSourceService(),
+    },
+  ],
   exports: [DataSourceService],
 })
 export class DataSourceModule {}
