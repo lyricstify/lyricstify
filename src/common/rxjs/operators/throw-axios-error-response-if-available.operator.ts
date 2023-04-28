2a2695460bc0b39ee24a75567260c00d5fd2cad9
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-28 23:51:30 +0700

    refactor(rxjs): remove overkill operator

--- src/common/rxjs/operators/throw-axios-error-response-if-available.operator.ts
+++ src/common/rxjs/operators/throw-axios-error-response-if-available.operator.ts
@@ -1,26 +1,24 @@
 import { isAxiosError } from 'axios';
 import chalk from 'chalk';
-import { catchError, MonoTypeOperatorFunction, pipe, throwError } from 'rxjs';
+import { catchError, MonoTypeOperatorFunction, pipe } from 'rxjs';
 
 export const throwAxiosErrorResponseIfAvailable = <T>(
   from: string,
 ): MonoTypeOperatorFunction<T> =>
   pipe(
-    catchError((error) => {
-      return throwError(() => {
-        if (isAxiosError(error)) {
-          const message = `${error.name}: ${
-            error.message
-          }\n\nResponse: ${JSON.stringify(
-            error.response?.data,
-          )}\n\nSource: ${from}\n\nPlease rerun the related command again or try to run ${chalk.inverse(
-            'lyricstify init',
-          )}. If this does not solve your problem feel free to open a new issue to https://github.com/lyricstify/lyricstify/issues/`;
+    catchError((error: Error) => {
+      if (isAxiosError(error)) {
+        const message = `${error.name}: ${
+          error.message
+        }\n\nResponse: ${JSON.stringify(
+          error.response?.data,
+        )}\n\nSource: ${from}\n\nPlease rerun the related command again or try to run ${chalk.inverse(
+          'lyricstify init',
+        )}. If this does not solve your problem feel free to open a new issue to https://github.com/lyricstify/lyricstify/issues/`;
 
-          return new Error(message);
-        }
+        throw new Error(message);
+      }
 
-        return new Error(error);
-      });
+      throw error;
     }),
   );
