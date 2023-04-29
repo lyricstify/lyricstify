af00d84264b5bcf65590e14199f99ae9aa02d9b6
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-29 10:34:35 +0700

    fix(transformation): update deps to google-translate-api-x for better stability

--- src/transformation/pipes/initialization-phase/google-translation-and-romanization.pipe.ts
+++ src/transformation/pipes/initialization-phase/google-translation-and-romanization.pipe.ts
@@ -1,8 +1,4 @@
-import {
-  Sentence,
-  SrcTranslit as SourceTranslation,
-} from '@vitalets/google-translate-api/dist/cjs/types.js';
-import { from } from 'rxjs';
+import { catchError, defer, of, retry } from 'rxjs';
 import { InitializationPipeFunction } from '../../interfaces/initialization-pipe-function.interface.js';
 
 interface GoogleTranslationOptions {
@@ -12,100 +8,62 @@ interface GoogleTranslationOptions {
   hideSourceLyrics: boolean;
 }
 
-const isSourceTranslation = (value: unknown): value is SourceTranslation =>
-  value !== undefined &&
-  value !== null &&
-  typeof value === 'object' &&
-  'src_translit' in value;
-
-const isSentence = (value: unknown): value is Sentence =>
-  value !== undefined &&
-  value !== null &&
-  typeof value === 'object' &&
-  'trans' in value &&
-  'orig' in value;
-
 export const googleTranslationAndRomanization = ({
   to,
   romanize,
   showTranslation,
   hideSourceLyrics,
 }: GoogleTranslationOptions): InitializationPipeFunction => {
-  const googleTranslateApi = import('@vitalets/google-translate-api');
+  const googleTranslateApi = import('google-translate-api-x');
 
   return (lines) => {
-    return from(
-      (async () => {
-        if (lines.length === 0) {
-          return [];
-        }
+    return defer(async () => {
+      if (lines.length === 0) {
+        return [];
+      }
 
-        const lyrics = lines
-          .map((line) => line.words.split('\n').at(0))
-          .join(' \r ');
+      const lyrics = lines.map((line) => line.words.split('\n').at(0) ?? '');
 
-        const translate = (await googleTranslateApi).translate;
-        const { raw } = await translate(lyrics, {
-          to,
-        });
+      const translate = (await googleTranslateApi).translate;
+      const translatedLyrics = await translate(lyrics, {
+        to,
+      });
 
-        const { translatedLyrics, romanizedLyrics } = raw.sentences.reduce(
-          (acc, val) => {
-            if (isSourceTranslation(val)) {
-              return {
-                translatedLyrics: acc.translatedLyrics,
-                romanizedLyrics: val.src_translit.split('\r'),
-              };
-            }
+      const translatedLines = lines.map((line, index) => {
+        const translation = translatedLyrics.at(index);
+        const translatedLyric = translation?.text;
+        const romanizedLyric = translation?.raw?.at(0)?.at(0);
+        const isTranslated =
+          showTranslation === true &&
+          typeof translatedLyric === 'string' &&
+          translatedLyric.replace(/\s/g, '') !== line.words.replace(/\s/g, '');
+        const isRomanized =
+          romanize === true &&
+          typeof romanizedLyric === 'string' &&
+          romanizedLyric.replace(/\s/g, '') !== line.words.replace(/\s/g, '');
 
-            if (isSentence(val)) {
-              return {
-                romanizedLyrics: acc.romanizedLyrics,
-                translatedLyrics: [
-                  ...acc.translatedLyrics,
-                  val.trans.replace(/\n/g, ''),
-                ],
-              };
-            }
+        return {
+          ...line,
+          words: ([] as string[])
+            .concat(
+              (isRomanized === true || isTranslated === true) &&
+                hideSourceLyrics === true
+                ? []
+                : line.words,
+            )
+            .concat(isRomanized ? romanizedLyric.trim() : [])
+            .concat(isTranslated ? translatedLyric.trim() : [])
+            .join('\n'),
+        };
+      });
 
-            return acc;
-          },
-          {
-            translatedLyrics: [] as string[],
-            romanizedLyrics: [] as string[],
-          },
-        );
-
-        const translatedLines = lines.map((line, index) => {
-          const translatedLyric = translatedLyrics.at(index);
-          const isTranslated =
-            showTranslation === true &&
-            translatedLyric !== undefined &&
-            translatedLyric.replace(/\s/g, '') !==
-              line.words.replace(/\s/g, '');
-          const romanizedLyric = romanizedLyrics.at(index);
-          const isRomanized =
-            romanize === true &&
-            romanizedLyric !== undefined &&
-            romanizedLyric.replace(/\s/g, '') !== line.words.replace(/\s/g, '');
-
-          return {
-            ...line,
-            words: ([] as string[])
-              .concat(
-                (isRomanized === true || isTranslated === true) &&
-                  hideSourceLyrics === true
-                  ? []
-                  : line.words,
-              )
-              .concat(isRomanized ? romanizedLyric.trim() : [])
-              .concat(isTranslated ? translatedLyric.trim() : [])
-              .join('\n'),
-          };
-        });
-
-        return translatedLines;
-      })(),
+      return translatedLines;
+    }).pipe(
+      retry({
+        count: 3,
+        delay: 1000,
+      }),
+      catchError(() => of(lines)),
     );
   };
 };
